dc4af971e9fda59d62f8b699a763c0d7b54890e0
Kamal-Dhital <kdhital143@gmail.com>
2025-03-20 15:11:02 +0545

    Add main function

---
+++ main.py
@@ -0,0 +1,14 @@
+from addition import addition
+from subtract import subtract
+from multiplication import multiply
+from division import division
+
+def main():
+    print("Addition: ", addition(5, 3))
+    print("Subtraction: ", subtract(5, 3))
+    print("Multiplication: ", multiply(5, 3))
+    print("Division: ", division(5, 3))
+
+if __name__ == "__main__":
+    main()
+    
